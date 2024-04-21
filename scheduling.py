1f125f4105c944b011eb7a3bc592adcfb6145052
shaurya rakesh <134362720+ShauryaCodes2004@users.noreply.github.com>
2024-04-21 15:23:17 +0530

    Add files via upload

---
+++ scheduling.py
@@ -0,0 +1,26 @@
+import schedule
+import pywhatkit
+
+
+
+
+from datetime import datetime , time , timedelta
+import time as tm
+def job() :
+    pywhatkit.sendwhatmsg("+919910693302", "reached!", 15, 10)
+
+
+
+
+
+
+
+
+schedule.every().day.at("15:10").do(job)
+
+
+
+while True :
+    schedule.run_pending()
+    tm.sleep(1)
+
